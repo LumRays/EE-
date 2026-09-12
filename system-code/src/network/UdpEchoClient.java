8ff486c29b6aeac4a8864074ed9fc93de47e427d
old-jiang <834638058@qq.com>
2026-09-12 21:00:38 +0800

    网络编程

---
+++ system-code/src/network/UdpEchoClient.java
@@ -0,0 +1,48 @@
+package network;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class UdpEchoClient {
+
+    // 创建 socket 对象
+    private DatagramSocket socket = null;
+    private String serverIp;
+    private int serverPort;
+
+    public UdpEchoClient(String serverIp, int serverPort) throws SocketException {
+        socket = new DatagramSocket();
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+    }
+
+    public void start() throws IOException {
+        System.out.println("client start!");
+        Scanner scanner = new Scanner(System.in);
+        // 用户通过控制台输入字符串 将其发给服务器 让服务器读取响应
+        while (true) {
+            System.out.print("-> ");
+            String request = scanner.next();
+            if (request.equals("exit")) {
+                break;
+            }
+            // 将用户输入的字符串构造成 UDP 数据报 进行发送
+            DatagramPacket requestPacket = new DatagramPacket(request.getBytes(), request.getBytes().length, InetAddress.getByName(this.serverIp), this.serverPort);
+            socket.send(requestPacket);
+            // 从服务器读取响应
+            DatagramPacket responsePacket = new DatagramPacket(new byte[1024], 1024);
+            socket.receive(responsePacket);
+            String response = new String(responsePacket.getData(), 0, responsePacket.getLength());
+            // 显示响应内容
+            System.out.println(response);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        UdpEchoClient client = new UdpEchoClient("127.0.0.1", 9999);
+        client.start();
+    }
+
+}
---
+++ system-code/src/network/UdpEchoServer.java
@@ -0,0 +1,46 @@
+package network;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UdpEchoServer {
+
+    private DatagramSocket socket = null;
+
+    public UdpEchoServer(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+
+    // 启动服务器
+    public void start() throws IOException {
+        System.out.println("server start!");
+        while (true) {
+            // 1. 读取请求并解析
+            DatagramPacket requestPacket = new DatagramPacket(new byte[1024], 1024);
+            socket.receive(requestPacket);
+            // 此处把二进制数据转成字符串
+            String request = new String(requestPacket.getData(), 0, requestPacket.getLength());
+            // 2. 根据请求计算响应
+            String response = process(request);
+            // 3. 将响应返回客户端  再构造一个 DatagramPacket
+            DatagramPacket responsePacket = new DatagramPacket(response.getBytes(), response.getBytes().length, requestPacket.getSocketAddress());
+            socket.send(responsePacket);
+            // 4. 打印日志
+            System.out.printf("[%s:%d] request: %s, response: %s\n", requestPacket.getAddress().toString(), requestPacket.getPort(), request, response);
+        }
+    }
+
+    private String process(String request) {
+        return request;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        UdpEchoServer server = new UdpEchoServer(9999);
+        server.start();
+
+    }
+
+}
